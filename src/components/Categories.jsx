a8d84a06b669f98e4dfbba084c92b6fc4a49870a
Alex <alex.zhelyazko@yandex.by>
2022-07-17 17:38:27 +0300

    add: categories chooser

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
+
+const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
 export const Categories = () => {
+  const [activeCategory, setActiveCategory]  = useState(0)
+  console.log(activeCategory);
     return <div className="categories">
     <ul>
-      <li className="active">Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
+      {categories.map((el, index) => {
+       return <li className = {activeCategory===index ? 'active' : ''} onClick={() => setActiveCategory(index)}>{el}</li>
+      })}
     </ul>
   </div>
 }
